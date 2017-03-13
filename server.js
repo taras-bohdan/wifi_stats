e38c528d727444089cce22e8c51b2128d3386eeb
Taras Bohdan <Taras B. xdr-xdr@hotmail.com>
2017-03-13 20:52:36 +0200

    added two basic services for users

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
-var path = require('path');
-var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 8080;
+const path = require('path');
+const bodyParser = require('body-parser');
+const express = require('express');
+const assert = require('assert');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const PORT = process.env.PORT || 8080;
+const url = 'mongodb://localhost:27017/wifi_statistics';
 
 // using webpack-dev-server and middleware in development environment
 if (process.env.NODE_ENV !== 'production') {
-	var webpackDevMiddleware = require('webpack-dev-middleware');
-	var webpackHotMiddleware = require('webpack-hot-middleware');
-	var webpack = require('webpack');
-	var config = require('./webpack.config');
-	var compiler = webpack(config);
+	let webpackDevMiddleware = require('webpack-dev-middleware'),
+		webpackHotMiddleware = require('webpack-hot-middleware'),
+		webpack = require('webpack'),
+		config = require('./webpack.config'),
+		compiler = webpack(config);
 
 	app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.publicPath}));
 	app.use(webpackHotMiddleware(compiler));
 }
 
 app.use(express.static(path.join(__dirname, 'dist')));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// parse application/json
+app.use(bodyParser.json());
 
 app.get('/', function (request, response) {
 	response.sendFile(__dirname + '/dist/index.html')
@@ -29,6 +37,50 @@ app.listen(PORT, function (error) {
 	}
 });
 
-app.get('/users', function (request, response) {
-	response.send("users");
+
+app.get('/users', function (req, res) {
+	MongoClient.connect(url, function (err, db) {
+		if (err != null) {
+			res.status(400).send("Cannot connect to DB!");
+		}
+		else {
+			console.log("Connected correctly to server");
+			getAllUsersInfo(db, function (data) {
+				res.send(data);
+				db.close();
+			});
+		}
+	});
 });
+
+app.post('/user', function (req, res) {
+	MongoClient.connect(url, function (err, db) {
+		if (err != null) {
+			res.status(400).send("Cannot connect to DB!");
+		}
+		else {
+			console.log("Connected correctly to DB");
+			addUserInfo(req.body, db, function () {
+				db.close();
+			});
+		}
+	});
+});
+
+let addUserInfo = function (userInfo, db, callback) {
+	db.collection('users').insertOne(userInfo, function (err) {
+		assert.equal(err, null);
+		console.log("Inserted a document into the users collection.");
+		callback();
+	});
+};
+
+let getAllUsersInfo = function (db, callback) {
+	let collection = db.collection('users');
+	collection.find({}).toArray(function (err, docs) {
+		assert.equal(err, null);
+		console.log("Found the following records");
+		console.log(docs);
+		callback(docs);
+	});
+};
