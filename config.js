0fa5ee78fb33af539c0faeed96cafb62ff13d524
Taras Bohdan <Taras B. xdr-xdr@hotmail.com>
2017-04-17 13:26:50 +0300

    small fix

--- config.js
+++ config.js
@@ -6,4 +6,4 @@ export const PORT = process.env.PORT || 8080;
 export const DB_URL = isProduction ? 'mongodb://db_admin:123qwe@ds123080.mlab.com:23080/heroku_k2k6l934' :
 	'mongodb://localhost:27017/wifi_statistics';
 
-export const SERVER_HOST = isProduction ? 'https://wifi-stats.herokuapp.com/' : 'http://localhost:' + PORT;
+export const SERVER_HOST = isProduction ? 'https://wifi-stats.herokuapp.com' : 'http://localhost:' + PORT;
