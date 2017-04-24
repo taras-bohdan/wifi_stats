055eb8c6542305dc2d1a0aad56058e439ee2e47c
Taras Bohdan <Taras B. xdr-xdr@hotmail.com>
2017-04-24 19:57:28 +0300

    Added Privacy Policy placeholder

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import { Route, Link } from 'react-router-dom'
+import {Route, Link} from 'react-router-dom'
 import Statistics from './Statistics';
 import Login from './Login/Login'
+import PrivacyPolicy from './PrivacyPolicy';
 
 const App = function () {
 	return (
@@ -9,11 +10,13 @@ const App = function () {
 			<ul>
 				<li><Link to="/">Home</Link></li>
 				<li><Link to="/login">Login</Link></li>
+				<li><Link to="/privacy">Privacy Policy</Link></li>
 			</ul>
 			<hr/>
 
 			<Route exact path="/" component={Statistics}/>
 			<Route path="/login" component={Login}/>
+			<Route path="/privacy" component={PrivacyPolicy}/>
 		</div>
 	);
 };
---
+++ src/components/PrivacyPolicy.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class PrivacyPolicy extends Component{
+	render() {
+		return (
+			<div>
+				Privacy Policy will be here
+			</div>
+		);
+	}
+}
+
+export default PrivacyPolicy;
