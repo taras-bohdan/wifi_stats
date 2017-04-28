9df72d35a4e76ec451bd5b655a0a8d9d970c9fcd
Taras Bohdan <Taras B. xdr-xdr@hotmail.com>
2017-04-29 00:29:31 +0300

    Added circles with users number

---
+++ src/components/LineChart/Circles.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+
+class Circles extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<g className="circles">{ this.props.data.map(renderCircles(this.props)) }</g>
+		)
+	}
+}
+
+const renderCircles = (props) => {
+	return (userData, index) => {
+		const circleProps = {
+			cx: props.xScale(userData.date),
+			cy: props.yScale(userData.usersCount),
+			r: 4,
+			key: `dot-${index}`
+		};
+		return <circle {...circleProps}>
+			<title>{userData.usersCount} users</title>
+		</circle>;
+	};
+};
+
+export default Circles;
--- src/components/LineChart/Path.js
+++ src/components/LineChart/Path.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import * as d3 from "d3";
+import Circles from './Circles';
 
 class Path extends Component {
 	constructor(props) {
@@ -15,31 +16,34 @@ class Path extends Component {
 
 	updateD3(newProps) {
 		// Set the ranges
-		let x = d3.scaleTime().range([this.props.dimensions.padding, newProps.dimensions.width - this.props.dimensions.padding * 2]),
-			y = d3.scaleLinear().range([newProps.dimensions.height - this.props.dimensions.padding, this.props.dimensions.padding]);
+		let xScale = d3.scaleTime().range([this.props.dimensions.padding, newProps.dimensions.width - this.props.dimensions.padding * 2]),
+			yScale = d3.scaleLinear().range([newProps.dimensions.height - this.props.dimensions.padding, this.props.dimensions.padding]);
 		// Scale the range of the data
-		x.domain(d3.extent(newProps.data, function (d) {
+		xScale.domain(d3.extent(newProps.data, function (d) {
 			return d.date;
 		}));
-		y.domain([0, d3.max(newProps.data, function (d) {
+		yScale.domain([0, d3.max(newProps.data, function (d) {
 			return d.usersCount;
 		})]);
 
 		this.valueLine = d3.line()
 			.x(function (d) {
-				return x(new Date(d.date));
+				return xScale(new Date(d.date));
 			})
 			.y(function (d) {
-				return y(d.usersCount);
-			})
-			// .curve(d3.curveBasis);
+				return yScale(d.usersCount);
+			});
+		this.scaleX = xScale;
+		this.scaleY = yScale;
 	}
 
 
 	render() {
-
 		return (
-			<path className="line" d={this.valueLine(this.props.data)}/>
+			<g>
+				<path className="line" d={this.valueLine(this.props.data)}/>
+				<Circles xScale={this.scaleX} yScale={this.scaleY} data={this.props.data}/>
+			</g>
 		)
 	}
 }
