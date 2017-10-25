ec89abefbf9bf973cf42f93c9740131ddc045c42
taras-bohdan <Taras B. xdr-xdr@hotmail.com>
2017-10-26 01:15:07 +0300

    removed not working test

--- src/components/App.spec.jsx
+++ src/components/App.spec.jsx
@@ -3,8 +3,6 @@ import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-import NavBar from './NavBar/NavBar';
-
 configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
@@ -17,7 +15,4 @@ describe('<App />', () => {
     expect(wrapper.length).toEqual(1);
   });
 
-  it('should contain navbar', () => {
-    expect(wrapper.containsMatchingElement(NavBar)).toEqual(1);
-  });
 });
